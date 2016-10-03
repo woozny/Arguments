7c876ed8182d53a04b200d9063d2513d8957c420
Tomek W <tom.wozniczka@gmail.com>
2016-10-03 06:31:01 +0200

    Commands - arg invocations are now counted within their own commands

--- src/main/java/commands/PingCommand.java
+++ src/main/java/commands/PingCommand.java
@@ -1,17 +1,21 @@
 package commands;
 
+import tools.counter.Counter;
 import tools.ping.Ping;
 
 public class PingCommand implements Command {
 
-    private Ping ping;
+    private final Ping ping;
+    private final Counter counter;
 
-    public PingCommand(Ping ping) {
+    public PingCommand(Ping ping, Counter counter) {
         this.ping = ping;
+        this.counter = counter;
     }
 
     @Override
     public void execute(String[] arguments) {
         System.out.println(ping.parseAndReturnArgument(arguments));
+        counter.addWord(arguments[0]);
     }
 }
